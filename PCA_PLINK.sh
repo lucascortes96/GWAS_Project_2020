1ad8611dc770e6c3057229c41990aad3f916e8e9
lucascortes96 <55763536+lucascortes96@users.noreply.github.com>
2020-02-10 09:59:48 -0500

    Create PCA_PLINK.sh

---
+++ PCA_PLINK.sh
@@ -0,0 +1,15 @@
+# Creating files used to create PCA plote from PLINK 
+
+
+# perform linkage pruning - i.e. identify prune sites. Use output files from variant calling 
+plink --vcf full_cohort.vcf --double-id --allow-extra-chr \
+--set-missing-var-ids @:# \
+--indep-pairwise 50 10 0.1 --out full_cohort
+
+## Making PCA via PLINK
+--vcf full_cohort.vcf --double-id --allow-extra-chr --set-missing-var-ids @:#
+--extract full_test.prune.in --make-bed --mind --threads 12
+--pca --out full_test_pca
+
+## This way allows you to exclude individuals from your cohort, make the text file with individuals you want to exclude
+plink --bfile full_cohort_plink --pca --allow-extra-chr --remove note_test.txt --mind 0.9
